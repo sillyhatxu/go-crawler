21dfa7ebb9b0cd88fbbf153457504e1bd7eb4f8d
shikuan.xu <shikuan.xu@advance.ai>
2019-10-04 18:40:07 +0800

    init

--- service/longmanword.go
+++ service/longmanword.go
@@ -1,8 +1,33 @@
 package service
 
-func AutoCrawlerLongmanWord() {
+import (
+	"github.com/sillyhatxu/word-backend/grpc/grpcclient"
+	"github.com/sillyhatxu/word-backend/grpc/word"
+)
 
+func AutoCrawlerLongmanWord() {
+	rows := 370103
+	pageSize := 5000
+	pageSum := (rows + pageSize - 1) / pageSize
+	for page := 0; page < pageSum; page++ {
+		offset := page * pageSize
+		limit := pageSize
+
+		//fmt.Println(array[start:end])
+	}
 }
+
+func Find(address string, offset, limit int64) (*word.WordListResponse, error) {
+	client := grpcclient.NewGRPCWordClient(address)
+	return client.WordList(&word.ListRequest{Offset: offset, Limit: limit})
+}
+
+//limit %d,%d ", params.Offset, params.Limit)
+//select * from vocabulary limit 0,10;
+//select * from vocabulary limit 10,10;
+//select * from vocabulary limit 20,10;
+//select * from vocabulary limit 30,10;
+//select * from vocabulary limit 40,10;
 
 //type CrawlerLongmanWord struct {
 //	Name string
